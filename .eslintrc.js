8be9b7c4aa118a25922536e4fa86361ec040c818
CandresFelipe <carrerafandres2496@gmail.com>
2024-08-15 02:00:22 +0200

    initial expo setup

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['expo', 'prettier', 'universe/native'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'max-len': ['error', { code: 80 }],
+  },
+};
